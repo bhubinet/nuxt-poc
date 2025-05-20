409fa09a2a766491f3850bd12c5ccbe94cd51347
HUBINET Brice <brice.hubinet@emundus.fr>
2025-05-20 16:43:05 +0200

    Remove useless files

--- pages/contacts/add.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <div>
-    <h1>Ajouter un contact</h1>
-    <p>Ci-dessous un formulaire pour ajouter un contact</p>
-  </div>
-</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,20 @@
 <template>
-  <div>
-    <Navigation />
-    <router-view  />
-    <h1 class="tw:text-neutral-500">Bienvenue sur la page d'accueil</h1>
-    <p class="tw:text-neutral-400">Voici une mini-SPA Nuxt intégrée dans Joomla.</p>
+  <div class="flex-col w-full h-screen bg-zinc-950 items-center justify-center flex">
+    <div class="mx-auto">
+      <h1 class="text-3xl text-blue-400 font-bold underline">
+        Nuxt 3 template
+      </h1>
+      <ul class="text-white flex flex-col gap-y-3 mt-6 ">
+        <li class="hover:text-blue-950">
+          <a href="https://www.typescriptlang.org/">Typescript</a>
+        </li>
+        <li class="hover:text-blue-950">
+          <a href="https://eslint.org/">Eslint</a>
+        </li>
+        <li class="hover:text-blue-950">
+          <a href="https://tailwindcss.com/">TailwindCSS</a>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
